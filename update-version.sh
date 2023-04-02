d80c773bae712b9b84043614f402f55e61a4c39e
Knut Ahlers <knut@ahlers.me>
2023-04-02 16:33:48 +0200

    Add update-version script
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ update-version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+### ---- ###
+
+echo "Switch back to master"
+git checkout master
+git reset --hard origin/master
+
+### ---- ###
+
+version=$(curl -s "https://lv.luzifer.io/v1/catalog/kubernetes/latest/version")
+grep -q "KUBECTL_VERSION=${version}$" Dockerfile && exit 0 || echo "Update required"
+
+sed -Ei \
+  -e "s/KUBECTL_VERSION=v[0-9.]+/KUBECTL_VERSION=v${version}/" \
+  Dockerfile
+
+### ---- ###
+
+echo "Testing build..."
+docker build .
+
+### ---- ###
+
+echo "Updating repository..."
+git add Dockerfile
+git -c user.name='Luzifer.io Jenkins' -c user.email='jenkins@luzifer.io' \
+  commit -m "kubectl ${version}"
+git tag ${version}
+
+git push -q https://${GH_USER}:${GH_TOKEN}@github.com/luzifer-docker/kubectl.git master --tags
